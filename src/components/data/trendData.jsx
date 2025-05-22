cc0d0d1c3e951abbe9d0f7be111fb06ced3bad91
LEEJUNGKWAN1 <wjdrhks1996@naver.com>
2025-05-22 23:20:36 +0900

    Code refactoring (parameterization)
    하드코딩 되어있는 컴포넌트 모두 변수화

---
+++ src/components/data/trendData.jsx
@@ -0,0 +1,28 @@
+const trendData = [
+  {
+    id: 1,
+    trendingIn: "Trending in Joseon",
+    itemName: "무인정사",
+    trendTweets: "20.7K posts",
+  },
+  {
+    id: 2,
+    trendingIn: "Trending in Joseon",
+    itemName: "박포의 난",
+    trendTweets: "18.1K posts",
+  },
+  {
+    id: 3,
+    trendingIn: "Trending in Joseon",
+    itemName: "한양 대화재",
+    trendTweets: "43.8K posts",
+  },
+  {
+    id: 4,
+    trendingIn: "Trending in Joseon",
+    itemName: "노비종모법",
+    trendTweets: "2,096 posts",
+  },
+];
+
+export default trendData;
