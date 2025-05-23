8ebb3b6a3c6a0e06e6314bbfc4ab3a10c689525a
minki1220 <130380152+minki1220@users.noreply.github.com>
2025-05-23 10:07:50 +0900

    add: 게시물 carousel 적용

--- src/pages/components/mainpage/ItemImages.jsx
+++ src/pages/components/mainpage/ItemImages.jsx
@@ -1,12 +1,34 @@
+import { useState } from "react";
+import Carousel from "../profilepage/Carousel";
 import { ItemsImgWrapper, ItemsImg } from "./styles";
 
 function ItemImages({ images }) {
+  const [isCarouselOpen, setIsCarouselOpen] = useState(false);
+  const [carouselIndex, setCarouselIndex] = useState(0);
+
+  const openCarousel = (index) => {
+    setCarouselIndex(index);
+    setIsCarouselOpen(true);
+  };
+
+  const closeCarousel = () => {
+    setIsCarouselOpen(false);
+  };
   return (
-    <ItemsImgWrapper>
-      {images.map((img, idx) => (
-        <ItemsImg src={img} key={idx} />
-      ))}
-    </ItemsImgWrapper>
+    <>
+      <ItemsImgWrapper>
+        {images.map((img, idx) => (
+          <ItemsImg src={img} key={idx} onClick={() => openCarousel(idx)} />
+        ))}
+      </ItemsImgWrapper>
+      {isCarouselOpen && (
+        <Carousel
+          images={images}
+          currentIndex={carouselIndex}
+          onClose={closeCarousel}
+        />
+      )}
+    </>
   );
 }
 
